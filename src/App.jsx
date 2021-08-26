f99ec674186b78472ceb06906b30eecb43ea3ea4
Ismael Albert <albertismael7@gmail.com>
2021-08-26 11:47:47 -0300

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import './styles/global.scss'
 
 export function App(){  
-    return <h1>hello react</h1>
+    return (
+        <main>
+        <h1>Olá</h1>
+        <p>Template CRA</p>
+        <p>toda as dependencias instaladas estão no arquivo 'note.txt'</p>
+        <div className="social">
+            <h3>Ismael Albert</h3>
+        <a href="https://www.instagram.com/ismaelalbert_/">instagram</a>
+        <a href="https://www.linkedin.com/in/ismaelalbert/">linkedin</a>
+        </div>
+        </main>
+    )
 }
